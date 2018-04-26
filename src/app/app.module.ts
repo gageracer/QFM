ec4e421371ff105bd79b060d5c21f1ddb8bc9cdb
gageracer <2171746+gageracer@users.noreply.github.com>
2018-04-26 22:52:34 +0300

    Routing test added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
@@ -9,7 +9,36 @@ import { environment } from '../environments/environment';
 import { HeaderComponent } from './header/header.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { ProfileComponent } from './mainpage/profile/profile.component';
+import { MessagesComponent } from './mainpage/messages/messages.component';
+import { MaterialsComponent } from './mainpage/materials/materials.component';
+import { SurveysComponent } from './mainpage/surveys/surveys.component';
+import { StatisticsComponent } from './mainpage/statistics/statistics.component';
+import { MymentorComponent } from './mainpage/mymentor/mymentor.component';
+import { MymenteesComponent } from './mainpage/mymentees/mymentees.component';
+import { MatchesComponent } from './mainpage/matches/matches.component';
+import { FaqComponent } from './mainpage/faq/faq.component';
+import { MentorsComponent } from './mainpage/mentors/mentors.component';
+import { MenteesComponent } from './mainpage/mentees/mentees.component';
+import { SettingsComponent } from './mainpage/settings/settings.component';
 
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/welcome', pathMatch: 'full'},
+  {path: 'main-page',   component: WelcomeComponent},
+  {path: 'profile',     component: ProfileComponent},
+  {path: 'messages',    component: MessagesComponent},
+  {path: 'materials',   component: MaterialsComponent},
+  {path: 'surveys',     component: SurveysComponent},
+  {path: 'statistics',  component: StatisticsComponent},
+  {path: 'mymentor',    component: MymentorComponent},
+  {path: 'mymentees',   component: MymenteesComponent},
+  {path: 'matches',     component: MatchesComponent},
+  {path: 'faq',         component: FaqComponent},
+  {path: 'mentors',     component: MentorsComponent},
+  {path: 'mentees',     component: MenteesComponent},
+  {path: 'settings',    component: SettingsComponent}
+
+];
 
 
 
@@ -23,6 +52,10 @@ import { MainpageComponent } from './mainpage/mainpage.component';
 ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],

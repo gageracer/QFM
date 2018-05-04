4dd5571a026c0627d18535f2a18dcf2e776f6b35
SAMİ ŞEN <samisen@SAMIs-MacBook-Pro.local>
2018-05-04 12:27:09 +0300

    same

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
+
 })
 export class WelcomeComponent implements OnInit {
 

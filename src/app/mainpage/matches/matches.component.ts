3831b8e936b3c56ad4baae4cce65088561afb378
SAMİ ŞEN <samisen@SAMIs-MacBook-Pro.local>
2018-05-23 23:06:59 +0300

    all

--- src/app/mainpage/matches/matches.component.ts
+++ src/app/mainpage/matches/matches.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./matches.component.css']
 })
 export class MatchesComponent implements OnInit {
-
+  isAlumniMentorCollapsed = true;
+  isSeniorMentorCollapsed = true;
+  isMatchCollapsed = true;
+  isSeniorMenteeCollapsed = true;
+  isUMatchSMenteeCollapsed = true;
+  isUMatchAlumniCollapsed = true;
+  isUMatchFreshmenCollapsed = true;
+  isUMatchSMentorCollapsed = true;
   constructor() { }
 
   ngOnInit() {

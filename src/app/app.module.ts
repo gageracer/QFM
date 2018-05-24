720258a75b3d1343dc5932e6f88fa3996305629d
SAMİ ŞEN <samisen@samis-mbp.mynet>
2018-05-24 04:03:58 +0300

    chng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import { MentorsComponent } from './mainpage/mentors/mentors.component';
 import { MenteesComponent } from './mainpage/mentees/mentees.component';
 import { RegisterComponent } from './register/register.component';
 import { FmenteesComponent } from './mainpage/fmentees/fmentees.component';
-import { FindmentorComponent } from './mainpage/findmentor/findmentor.component';
+
 
 
 const appRoutes: Routes = [
@@ -56,7 +56,6 @@ export const routing = RouterModule.forRoot(appRoutes);
     MenteesComponent,
     RegisterComponent,
     FmenteesComponent,
-    FindmentorComponent,
   ],
 
   imports: [
